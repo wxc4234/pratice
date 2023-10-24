564156e9cd76a9362f5e1c323bc28f67e5328d89
wxc4234 <2735229038@qq.com>
2023-10-24 23:55:53 +0800

    罗马数字转整数

--- romanToInt.js
+++ romanToInt.js
@@ -2,32 +2,26 @@
  * @param {string} s
  * @return {number}
  */
+
 var romanToInt = function(s) {
-    const map = {
-        I: 1,
-        IV: 4,
-        V: 5,
-        IX: 9,
-        X: 10,
-        XL: 40,
-        L: 50,
-        XC: 90,
-        C: 100,
-        CD: 400,
-        D: 500,
-        CM: 900,
-        M: 1000
-    };
-    
-    let sum = 0;
-    for(let i = 0; i < s.length; ) {
-        if(i+1<s.length&&map[s.substring(i,i+2)])
-        {sum +=map[s.substring(i,i+2)]
-        i += 2;}
-        else {
-            sum += map[s.substring(i,i+1)];
-            i++;
-        }
+    let math={
+        I:1,
+        V:5,
+        X:10,
+        L:50,
+        C:100,
+        D:500,
+        M:1000,
     }
-     return sum ;
-    };
+   var arr = s.split('') 
+    var num=0
+    var i
+    for (i in arr) {
+        if(math[arr[i]]<math[arr[Number(i)+1]]) {
+            num=num-math[arr[i]]  
+    }else{
+        num=num+math[arr[i]]
+    }
+}
+return num
+}
