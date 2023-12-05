b980a3a818054182334bfe494a6c1b0157bf7264
wxc4234 <2735229038@qq.com>
2023-12-05 22:03:39 +0800

    无重复字符的最长子串

---
+++ middle/lengthOfLongestSubstring.js
@@ -0,0 +1,19 @@
+var lengthOfLongestSubstring = function(str) {
+  if (str.length <= 1) {return str.length}
+    let left = 0
+    let right = 1
+    let max = 0
+    let temp
+    while(right<str.length){
+      temp = str.slice(left, right)
+      if (temp.indexOf(str.charAt(right)) > -1) {
+        left++
+        continue
+      } else {
+        right++
+      }
+      if (right - left > max) { max = right - left }
+    }
+    return max
+};
+
