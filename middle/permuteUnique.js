0409927184e35d0dcc88d076cf0f8e4bdebf0388
wxc4234 <2735229038@qq.com>
2024-01-02 21:31:40 +0800

    全排列2

---
+++ middle/permuteUnique.js
@@ -0,0 +1,31 @@
+
+var permuteUnique = function (nums) {
+  nums.sort((a, b) => {
+      return a - b
+  })
+  let result = []
+  let path = []
+
+  function backtracing( used) {
+      if (path.length === nums.length) {
+          result.push([...path])
+          return
+      }
+      for (let i = 0; i < nums.length; i++) {
+          if (i > 0 && nums[i] === nums[i - 1] && !used[i - 1]) {
+              continue
+          }
+          if (!used[i]) {
+              used[i] = true
+              path.push(nums[i])
+              backtracing(used)
+              path.pop()
+              used[i] = false
+          }
+
+
+      }
+  }
+  backtracing([])
+  return result
+};
