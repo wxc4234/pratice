b293ec1c78d5b185a25c871630c9ea71c4d76465
wxc4234 <2735229038@qq.com>
2024-02-03 20:02:19 +0800

    组合总和

---
+++ middle/combinationSum.js
@@ -0,0 +1,21 @@
+var combinationSum = function(candidates, target) {
+  const res = [], path = [];
+  candidates.sort((a,b)=>a-b); // 排序
+  backtracking(0, 0); // 从0开始
+  return res;
+  function backtracking(j, sum) {
+      if (sum === target) {
+          res.push(Array.from(path));
+          return;
+      }
+      for(let i = j; i < candidates.length; i++ ) {
+          const n = candidates[i];
+          if(n > target - sum) break;
+          path.push(n);
+          sum += n;
+          backtracking(i, sum);
+          path.pop();
+          sum -= n;
+      }
+  }
+};
