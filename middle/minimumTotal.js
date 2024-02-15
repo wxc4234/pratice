0603ec4c8d3328a8c118345a1fc07275b31d6bde
wxc4234 <2735229038@qq.com>
2024-02-15 20:06:52 +0800

    三角形最小路径和

---
+++ middle/minimumTotal.js
@@ -0,0 +1,15 @@
+const minimumTotal = (triangle) => {
+  const bottom = triangle[triangle.length - 1];
+  const dp = new Array(bottom.length);
+  // base case 是最后一行
+  for (let i = 0; i < dp.length; i++) {
+    dp[i] = bottom[i];
+  }
+  // 从倒数第二列开始迭代
+  for (let i = dp.length - 2; i >= 0; i--) {
+    for (let j = 0; j < triangle[i].length; j++) {
+      dp[j] = Math.min(dp[j], dp[j + 1]) + triangle[i][j];
+    }
+  }
+  return dp[0];
+};
