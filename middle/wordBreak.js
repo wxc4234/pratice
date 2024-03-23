685d3ab38c42985b34d43a4b7cadea58d3fc74af
wxc4234 <2735229038@qq.com>
2024-03-23 21:50:05 +0800

    单词拆分

--- middle/wordBreak.js
+++ middle/wordBreak.js
@@ -1,19 +1,17 @@
 const wordBreak = (s, wordDict) => {
-	const len = s.length;
-  	const wordSet = new Set(wordDict);
+  const wordSet = new Set(wordDict);
+  const len = s.length;
+  const dp = new Array(len + 1).fill(false);
+  dp[0] = true;
 
-	const canBreak = (start) => { // 判断从start到末尾的子串能否break
-		if (start == len) {//指针越界，s一步步成功划分为单词，才走到越界这步，现在没有剩余子串
-			return true;   //返回真，结束递归
-		}
-		for (let i = start + 1; i <= len; i++) { //指针i去划分两部分，for枚举出当前所有的选项i
-			const prefix = s.slice(start, i);    // 切出的前缀部分
-			if (wordSet.has(prefix) && canBreak(i)) {// 前缀部分是单词，且剩余子串能break，返回真
-				return true;
-			} // 如果前缀部分不是单词，就不会执行canBreak(i)。进入下一轮迭代，再切出一个前缀串，再试
-		}
-		return false; // 指针i怎么划分，都没有返回true，则返回false
-	}
-
-	return canBreak(0); // 递归的入口，从0到末尾的子串能否break
-}
+  for (let i = 1; i <= len; i++) {
+    for (let j = i - 1; j >= 0; j--) {    // j去划分成两部分
+      const suffix = s.slice(j, i);       // 后缀部分 s[j: i-1]
+      if (wordSet.has(suffix) && dp[j]) { // 后缀部分是单词，且左侧子串[0,j-1]的dp[j]为真
+        dp[i] = true;
+        break;  // dp[i] = true了，i长度的子串已经可以拆成单词了，不需要j继续划分子串了
+      }
+    }
+  }
+  return dp[len];
+};
